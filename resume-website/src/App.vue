09528f5e0ddc25d8c3b12076fe5db1ad5c0ed78d
Arthur Jakobsson <21arthurj@students.harker.org>
2020-02-29 21:04:55 -0800

    Vue Structure

---
+++ resume-website/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="message"/>
+    <input type="text" v-model="message"/>
+    <button @click="alertMessage">Alert</button>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      message: 'Title'
+    }
+  },
+  methods: {
+      alertMessage(){
+        alert(this.message)
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
